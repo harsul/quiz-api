7f802e10cc35f1ce50b663023ecd24790e7d9087
Harun Suljagic <harun.suljagic@sleepnumber.com>
2022-09-25 17:11:27 +0200

    Delete quiz command and handler added

---
+++ BackendCandidateChallenge/QuizService.Application/Features/Quizzes/Commands/DeleteQuizCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+using QuizService.Application.Models;
+using QuizService.Domain.Models;
+
+namespace QuizService.Application.Features.Quizzes.Commands
+{
+    public class DeleteQuizCommand : IRequest<OperationResult<Quiz>>
+    {
+        public int Id { get; set; }
+    }
+}
