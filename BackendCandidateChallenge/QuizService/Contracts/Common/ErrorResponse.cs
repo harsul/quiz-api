b4727338e1016e45e9db677ddf00cb408aa257b6
Harun Suljagic <harun.suljagic@sleepnumber.com>
2022-09-25 17:11:38 +0200

    Error response model added

---
+++ BackendCandidateChallenge/QuizService/Contracts/Common/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace QuizService.Contracts.Common
+{
+    public class ErrorResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; } = new();
+        public DateTime Timestamp { get; set; }
+    }
+}
