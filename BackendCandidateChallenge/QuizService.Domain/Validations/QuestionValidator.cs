cb83f5bab337a8e9a4cff07e1ad2c91c7dd27ce0
Harun Suljagic <harun.suljagic@sleepnumber.com>
2022-09-25 15:19:52 +0200

    Validation for objects added

---
+++ BackendCandidateChallenge/QuizService.Domain/Validations/QuestionValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+using QuizService.Domain.Models;
+
+namespace QuizService.Domain.Validations;
+
+public class QuestionValidator : AbstractValidator<Question>
+{
+    public QuestionValidator()
+    {
+        RuleFor(question => question.Text).NotNull().WithMessage("Text content could not be null")
+            .NotEmpty().WithMessage("Text content could not be empty");
+    }
+}
+
---
+++ BackendCandidateChallenge/QuizService.Domain/Validations/QuizValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+using QuizService.Domain.Models;
+
+namespace QuizService.Domain.Validations;
+
+public class QuizValidator : AbstractValidator<Quiz>
+{
+    public QuizValidator()
+    {
+        RuleFor(quiz => quiz.Title).NotNull().WithMessage("Text content could not be null")
+            .NotEmpty().WithMessage("Text content could not be empty");
+    }
+}
+
